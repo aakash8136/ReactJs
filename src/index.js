5df42ed6103f72dfe7639b75917d5642d709321a
Aakash Goel <Aakash.Goel@Ltts.com>
2019-12-11 17:59:31 +0530

    'API_Requests'

--- src/index.js
+++ src/index.js
@@ -1,61 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
- import './index.css';
- import Spinner from './Spinner';
+import './index.css';
+import Spinner from './Spinner';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import SeasonDisplay from './SeasonDisplay';
 
-class App extends React.Component{
-    state={ lat:null , errorMessage:''};
+class App extends React.Component {
+    state = { lat: null, errorMessage: '' };
 
-    componentDidMount(){
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude}),
-            err => this.setState({errorMessage: err.message})
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
 
-    renderContent(){
-        if(this.state.errorMessage && !this.state.lat)
-        {
-        return <div>error : {this.state.errorMessage}</div>;
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>error : {this.state.errorMessage}</div>;
         }
-    
-        if(!this.state.errorMessage && this.state.lat)
-        {
-        //return <div>latitude : {this.state.lat}</div>;
-        return <SeasonDisplay lat={this.state.lat} />;
+
+        if (!this.state.errorMessage && this.state.lat) {
+            //return <div>latitude : {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
         }
-    
-        return <Spinner message="please accept location request "/>;
+
+        return <Spinner  />;
         //return <div>loading!</div>;
     }
-    
 
-render() {
-    return (
-        <div className="border red">
-            {this.renderContent()}
-        </div>
-    );
-    // if(this.state.errorMessage && !this.state.lat)
-    // {
-    // return <div>error : {this.state.errorMessage}</div>;
-    // }
 
-    // if(!this.state.errorMessage && this.state.lat)
-    // {
-    // //return <div>latitude : {this.state.lat}</div>;
-    // return <SeasonDisplay lat={this.state.lat} />;
-    // }
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
+        // if(this.state.errorMessage && !this.state.lat)
+        // {
+        // return <div>error : {this.state.errorMessage}</div>;
+        // }
 
-    // return <Spinner message="please accept location request "/>;
-    // //return <div>loading!</div>;
+        // if(!this.state.errorMessage && this.state.lat)
+        // {
+        // //return <div>latitude : {this.state.lat}</div>;
+        // return <SeasonDisplay lat={this.state.lat} />;
+        // }
+
+        // return <Spinner message="please accept location request "/>;
+        // //return <div>loading!</div>;
+    }
 }
-}
 
-ReactDOM.render(<App />, document.getElementById('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
 
 
 // If you want your app to work offline and load faster, you can change
